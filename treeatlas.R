f11e87a35c9690f3d3c77648f08758fafac0f415
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2019-03-17 19:00:36 -0400

    Extensive analysis

--- treeatlas.R
+++ treeatlas.R
@@ -4,7 +4,21 @@ library(rgdal)
 library(velox)
 library(lattice)
 library(dplyr)
+library(ggplot2)
 treelist <- read.delim('treefiles.txt')
+forms <- read.delim('data/NorthAmericaGrowthHabits20180924.txt', encoding="Latin1")
+syns <- read.delim('data/BONAPGRIN_NAMES.txt', encoding="UTF-8")
+colnames(syns) <- c("Binomial","Taxon","B_Taxon","B_Binomial","G_Taxon","G_Binomial") 
+formsyns <- merge(syns, forms, by.x='Binomial')
+formsyns <- unique(formsyns[!is.na(formsyns$B_Binomial) & !is.na(formsyns$HabitSymbol)& !formsyns$B_Binomial %in% "", c('HabitSymbol','B_Binomial')])
+treelistsyns <- merge(treelist, syns, by.x='Latin.Name', by.y='Binomial', all.x=TRUE)
+treelistsyns <- unique(treelistsyns[!is.na(treelistsyns$B_Binomial),c('name','B_Binomial')])
+treelistforms <- merge(treelistsyns, formsyns, by='B_Binomial', all.x=TRUE)
+BE <- as.character(unique(treelistforms[grepl('BE', treelistforms$HabitSymbol),'name']))
+BD <- as.character(unique(treelistforms[grepl('BD', treelistforms$HabitSymbol),'name']))
+NE <- as.character(unique(treelistforms[grepl('NE', treelistforms$HabitSymbol)|grepl('ND', treelistforms$HabitSymbol),'name']))
+unique(treelistforms$HabitSymbol)
+
 Tclx <- raster('nam/Tclx.tif')
 Tgs <- raster('nam/Tgs.tif')
 Tc <- raster('nam/Tc.tif')
@@ -12,68 +26,93 @@ MAP <- raster('nam/MAP.tif')
 Deficit <- raster('nam/Deficit.tif')
 Surplus <- raster('nam/Surplus.tif')
 pAET <- raster('nam/pAET.tif')
+slope <- raster('nam/slope.tif')
+sand <- raster('nam/sand.tif')
+SoilpH <- raster('nam/SoilpH.tif')
+hydric <- raster('nam/hydric.tif')
+salids <- raster('nam/salids.tif')
+
 M <- MAP/(Deficit + MAP - Surplus)
-n <- rownames(treelist[grepl('cyrirace', treelist$name),])
+vTclx <- velox(Tclx)
+vTgs <- velox(Tgs)
+vTc <- velox(Tc)
+vM <- velox(M)
+vDeficit <- velox(Deficit)
+vSurplus <- velox(Surplus)
+vpAET <- velox(pAET)
+vslope <- velox(slope)
+vsand <- velox(sand)
+vSoilpH<- velox(SoilpH)
+vhydric<- velox(hydric)
+vsalids<- velox(salids)
+line <- cbind(file = 'name',taxon = 'name',name = 'name', Tclx_max=0, Tclx_min=0, Tgs_max=0, Tgs_min=0)
+
+top = 0.98
+bot = 0.02
+
+
+
+n <- rownames(treelist[grepl('liqustyr', treelist$name),])
+
 spp<-read_sf(treelist[n,1])
 spp2 <- 	st_transform(spp, st_crs(Tclx))
 spp2 <- spp2[spp2$CODE %in% '1',]
 
-plot(Tclx, main=treelist[n,4])
-plot(st_geometry(spp2), add=T)
-top = 0.98
-bot = 0.02
+#plot(Tclx, main=treelist[n,4])
+#plot(st_geometry(spp2), add=T)
+
 
 #Tclx----
-vx <- velox(Tclx)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vTclx$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
-densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tclx', ylab='Density', main='')
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tclx', ylab='Density', main='')
 Tclx_max <- quantile(e[,2], top)
 Tclx_min <- quantile(e[,2], bot)
 
 #Tgs----
-vx <- velox(Tgs)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vTgs$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
 #densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tgs', ylab='Density', main='')
 Tgs_max <- quantile(e[,2], top)
 Tgs_min <- quantile(e[,2], bot)
 
 #Tc----
-vx <- velox(Tc)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vTc$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
 #densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tc', ylab='Density', main='')
 Tc_max <- quantile(e[,2], top)
 Tc_min <- quantile(e[,2], bot)
 
 #M----
-vx <- velox(M)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vM$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
 #densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='M', ylab='Density', main='')
 M_max <- quantile(e[,2], top)
 M_min <- quantile(e[,2], bot)
 
 #Surplus----
-vx <- velox(Surplus)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vSurplus$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
 #densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Surplus', ylab='Density', main='')
 Surplus_max <- quantile(e[,2], top)
 Surplus_min <- quantile(e[,2], bot)
 
 #Deficit----
-vx <- velox(Deficit)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vDeficit$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
 #densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Deficit', ylab='Density', main='')
 Deficit_max <- quantile(e[,2], top)
 Deficit_min <- quantile(e[,2], bot)
 
 #pAET----
-vx <- velox(pAET)
-e <- vx$extract(spp2, df=T, small = T)
+
+e <- vpAET$extract(spp2, df=T, small = T)
 e <- e[!is.na(e[,2]),]
 #densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
 
@@ -81,12 +120,87 @@ pAET_max <- quantile(e[,2], top)
 pAET_min <- quantile(e[,2], bot)
 
 
-envelop <- (Tclx <= Tclx_max)*(Tclx >= Tclx_min)*
+#slope----
+
+e <- vslope$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+slope_max <- quantile(e[,2], top)
+slope_min <- quantile(e[,2], bot)
+
+#sand----
+
+e <- vsand$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+sand_max <- quantile(e[,2], top)
+sand_min <- quantile(e[,2], bot)
+
+#SoilpH----
+
+e <- vSoilpH$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+SoilpH_max <- quantile(e[,2], top)
+SoilpH_min <- quantile(e[,2], bot)
+
+#hydric----
+
+e <- vhydric$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+hydric_max <- quantile(e[,2], top)
+hydric_min <- quantile(e[,2], bot)
+
+#salids----
+
+e <- vsalids$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+salids_max <- quantile(e[,2], top)
+salids_min <- quantile(e[,2], bot)
+
+
+envelopT <- (Tclx <= Tclx_max)*(Tclx >= Tclx_min)*
   (Tgs <= Tgs_max)*(Tgs >= Tgs_min)*
-  (M <= M_max)*(M >= M_min)*
+  (Tc <= Tc_max)*(Tc >= Tc_min)
+
+envelopM <- (M <= M_max)*(M >= M_min)*
   (pAET <= pAET_max)*(pAET >= pAET_min)*
-  (Tc <= Tc_max)*(Tc >= Tc_min)*
   (Deficit <= Deficit_max)*(Deficit >= Deficit_min)*
   (Surplus <= Surplus_max)*(Surplus >= Surplus_min)
-plot(envelop)
+
+envelopS <- (slope <= slope_max)*(slope >= slope_min)*
+  (sand <= sand_max)*(sand >= sand_min)*
+  (SoilpH <= SoilpH_max)*(SoilpH >= SoilpH_min)*
+  (hydric <= hydric_max)*(hydric >= hydric_min)*
+  (salids <= salids_max)*(salids >= salids_min)
+envelop3 <- envelopT*envelopM*envelopS
+
+
+
+minusTclx <- (slope <= slope_max)*(slope >= slope_min)*
+  (sand <= sand_max)*(sand >= sand_min)*
+  (SoilpH <= SoilpH_max)*(SoilpH >= SoilpH_min)*
+  (hydric <= hydric_max)*(hydric >= hydric_min)*
+  (salids <= salids_max)*(salids >= salids_min)*
+  (Tclx <= Tclx_max)*(Tclx >= Tclx_min)*
+  (Tgs <= Tgs_max)*(Tgs >= Tgs_min)*
+  (Tc <= Tc_max)*(Tc >= Tc_min)*
+  (M <= M_max)*(M >= M_min)*
+  (pAET <= pAET_max)*(pAET >= pAET_min)*
+  (Deficit <= Deficit_max)*(Deficit >= Deficit_min)*
+  (Surplus <= Surplus_max)*(Surplus >= Surplus_min)
+
+diff <- minusTclx-envelop3
+cellStats(diff, stat='mean')
+plot(diff, main=treelist[n,3])
+
+plot(envelop3, main=treelist[n,3])
 plot(st_geometry(spp2), add=T)
+
---
+++ treeatlas_buildtable.R
@@ -0,0 +1,416 @@
+library(sf)
+library(raster)
+library(rgdal)
+library(velox)
+library(lattice)
+library(dplyr)
+library(ggplot2)
+treelist <- read.delim('treefiles.txt')
+forms <- read.delim('data/NorthAmericaGrowthHabits20180924.txt', encoding="Latin1")
+syns <- read.delim('data/BONAPGRIN_NAMES.txt', encoding="UTF-8")
+colnames(syns) <- c("Binomial","Taxon","B_Taxon","B_Binomial","G_Taxon","G_Binomial") 
+formsyns <- merge(syns, forms, by.x='Taxon', by.y='Scientific.Name')
+formsyns <- unique(formsyns[!is.na(formsyns$B_Binomial) & !is.na(formsyns$HabitSymbol)& !formsyns$B_Binomial %in% "", c('HabitSymbol','B_Binomial')])
+treelistsyns <- merge(treelist, syns, by.x='Latin.Name', by.y='Taxon', all.x=TRUE)
+treelistsyns <- unique(treelistsyns[!is.na(treelistsyns$B_Binomial),c('name','B_Binomial')])
+treelistforms <- merge(treelistsyns, formsyns, by='B_Binomial', all.x=TRUE)
+BE <- as.character(unique(treelistforms[grepl('BE', treelistforms$HabitSymbol),'name']))
+PALM <- as.character(unique(treelistforms[grepl('P', treelistforms$HabitSymbol),'name']))
+BD <- as.character(unique(treelistforms[grepl('BD', treelistforms$HabitSymbol),'name']))
+NE <- as.character(unique(treelistforms[grepl('NE', treelistforms$HabitSymbol)|grepl('ND', treelistforms$HabitSymbol),'name']))
+SU <- as.character(unique(treelistforms[grepl('U', treelistforms$HabitSymbol),'name']))
+
+unique(treelistforms$HabitSymbol)
+
+Tclx <- raster('nam/Tclx.tif')
+Tgs <- raster('nam/Tgs.tif')
+Tc <- raster('nam/Tc.tif')
+MAP <- raster('nam/MAP.tif')
+Deficit <- raster('nam/Deficit.tif')
+Surplus <- raster('nam/Surplus.tif')
+pAET <- raster('nam/pAET.tif')
+slope <- raster('nam/slope.tif')
+sand <- raster('nam/sand.tif')
+SoilpH <- raster('nam/SoilpH.tif')
+hydric <- raster('nam/hydric.tif')
+salids <- raster('nam/salids.tif')
+
+M <- MAP/(Deficit + MAP - Surplus)
+vTclx <- velox(Tclx)
+vTgs <- velox(Tgs)
+vTc <- velox(Tc)
+vM <- velox(M)
+vDeficit <- velox(Deficit)
+vSurplus <- velox(Surplus)
+vpAET <- velox(pAET)
+vslope <- velox(slope)
+vsand <- velox(sand)
+vSoilpH<- velox(SoilpH)
+vhydric<- velox(hydric)
+vsalids<- velox(salids)
+line <- cbind(file = 'name',taxon = 'name',name = 'name', Tclx_max=0, Tclx_min=0, Tgs_max=0, Tgs_min=0)
+
+top = 0.98
+bot = 0.02
+
+for(i in 1:nrow(treelist)){
+
+#n <- rownames(treelist[grepl('larrdiva', treelist$name),])
+n <- i
+spp<-read_sf(treelist[n,1])
+spp2 <- 	st_transform(spp, st_crs(Tclx))
+spp2 <- spp2[spp2$CODE %in% '1',]
+
+#plot(Tclx, main=treelist[n,4])
+#plot(st_geometry(spp2), add=T)
+
+
+#Tclx----
+
+e <- vTclx$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tclx', ylab='Density', main='')
+Tclx_max <- quantile(e[,2], top)
+Tclx_min <- quantile(e[,2], bot)
+
+#Tgs----
+
+e <- vTgs$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tgs', ylab='Density', main='')
+Tgs_max <- quantile(e[,2], top)
+Tgs_min <- quantile(e[,2], bot)
+
+line2 <- cbind(file = as.character(treelist[n,2]), taxon = as.character(treelist[n,3]),
+               name = as.character(treelist[n,4]), Tclx_max, Tclx_min, Tgs_max, Tgs_min)
+line <- rbind(line, line2)
+
+}
+
+#Tc----
+
+e <- vTc$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Tc', ylab='Density', main='')
+Tc_max <- quantile(e[,2], top)
+Tc_min <- quantile(e[,2], bot)
+
+#M----
+
+e <- vM$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='M', ylab='Density', main='')
+M_max <- quantile(e[,2], top)
+M_min <- quantile(e[,2], bot)
+
+#Surplus----
+
+e <- vSurplus$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Surplus', ylab='Density', main='')
+Surplus_max <- quantile(e[,2], top)
+Surplus_min <- quantile(e[,2], bot)
+
+#Deficit----
+
+e <- vDeficit$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='Deficit', ylab='Density', main='')
+Deficit_max <- quantile(e[,2], top)
+Deficit_min <- quantile(e[,2], bot)
+
+#pAET----
+
+e <- vpAET$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+pAET_max <- quantile(e[,2], top)
+pAET_min <- quantile(e[,2], bot)
+
+
+#slope----
+
+e <- vslope$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+slope_max <- quantile(e[,2], top)
+slope_min <- quantile(e[,2], bot)
+
+#sand----
+
+e <- vsand$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+sand_max <- quantile(e[,2], top)
+sand_min <- quantile(e[,2], bot)
+
+#SoilpH----
+
+e <- vSoilpH$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+SoilpH_max <- quantile(e[,2], top)
+SoilpH_min <- quantile(e[,2], bot)
+
+#hydric----
+
+e <- vhydric$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+hydric_max <- quantile(e[,2], top)
+hydric_min <- quantile(e[,2], bot)
+
+#salids----
+
+e <- vsalids$extract(spp2, df=T, small = T)
+e <- e[!is.na(e[,2]),]
+#densityplot(e[,2], plot.points=FALSE, bw=2, lwd=2, col='RoyalBlue', xlab='pAET', ylab='Density', main='')
+
+salids_max <- quantile(e[,2], top)
+salids_min <- quantile(e[,2], bot)
+
+
+envelopT <- (Tclx <= Tclx_max)*(Tclx >= Tclx_min)*
+  (Tgs <= Tgs_max)*(Tgs >= Tgs_min)*
+  (Tc <= Tc_max)*(Tc >= Tc_min)
+
+envelopM <- (M <= M_max)*(M >= M_min)*
+  (pAET <= pAET_max)*(pAET >= pAET_min)*
+  (Deficit <= Deficit_max)*(Deficit >= Deficit_min)*
+  (Surplus <= Surplus_max)*(Surplus >= Surplus_min)
+
+envelopS <- (slope <= slope_max)*(slope >= slope_min)*
+  (sand <= sand_max)*(sand >= sand_min)*
+  (SoilpH <= SoilpH_max)*(SoilpH >= SoilpH_min)*
+  (hydric <= hydric_max)*(hydric >= hydric_min)*
+  (salids <= salids_max)*(salids >= salids_min)
+hist(envelopM)
+envelop3 <- envelopT*envelopM*envelopS
+plot(envelop3)
+plot(st_geometry(spp2), add=T)
+
+TreeClim <- as.data.frame(line)
+TreeClim$Tclx_min <- as.numeric(as.character(TreeClim$Tclx_min))
+TreeClim$Tclx_max <- as.numeric(as.character(TreeClim$Tclx_max))
+TreeClim$Tgs_min <- as.numeric(as.character(TreeClim$Tgs_min))
+TreeClim$Tgs_max <- as.numeric(as.character(TreeClim$Tgs_max))
+#saveRDS(TreeClim, 'data/TreeClim.RDS')
+#TreeClim <- readRDS('data/TreeClim.RDS')
+TreeClim <- unique(TreeClim[2:nrow(TreeClim),])
+rownames(TreeClim) <- TreeClim$file
+BroadEvergreens <- unique(TreeClim[TreeClim$file %in% BE,])
+Monocot <- unique(TreeClim[TreeClim$file %in% PALM,])
+Succulent <- unique(TreeClim[TreeClim$file %in% SU,])
+BroadDeciduous <- unique(TreeClim[TreeClim$file %in% BD,])
+Needle <- unique(TreeClim[TreeClim$file %in% NE,])
+TreeClim$file <- as.character(TreeClim$file)
+densityplot(Needle$Tclx_min)
+densityplot(BroadDeciduous$Tclx_min, add=T)
+densityplot(BroadEvergreens$Tclx_min, add=T)
+
+ggplot() + 
+  geom_density(data = BroadEvergreens, 
+               mapping = aes(x = Tclx_min, fill='Broadleaf Evergreen', color='Broadleaf Evergreen'),size=1, alpha=0.1)+
+  geom_density(data = BroadDeciduous, 
+               mapping = aes(x = Tclx_min, fill='Broadleaf Deciduous', color='Broadleaf Deciduous'), size=1, alpha=0.1)+
+  geom_density(data = Monocot, 
+               mapping = aes(x = Tclx_min, fill='Monocot', color='Monocot'), size=1, alpha=0.1)+
+  geom_density(data = Succulent, 
+               mapping = aes(x = Tclx_min, fill='Succulent', color='Succulent'), size=1, alpha=0.1)+
+  geom_density(data = Needle, 
+               mapping = aes(x = Tclx_min, fill='Needleleaf', color='Needleleaf'), size=1, alpha=0.1)+
+  scale_fill_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                         "Broadleaf Deciduous" = "green",
+                                         "Monocot" = "yellow",
+                                         "Succulent" = "purple",
+                                         "Needleleaf" = "blue"))+
+  scale_color_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                          "Broadleaf Deciduous" = "dark green",
+                                          "Monocot" = "orange",
+                                          "Succulent" = "purple",
+                                          "Needleleaf" = "blue"))+
+  scale_x_continuous(name= "Minnimum Annual Extreme Low", breaks=seq(-65, 5, by = 5))+
+  scale_y_continuous(name= "Tree Species Density")
+
+ggplot() + 
+  geom_density(data = BroadEvergreens, 
+               mapping = aes(x = Tgs_min, fill='Broadleaf Evergreen', color='Broadleaf Evergreen'),size=1, alpha=0.1)+
+  geom_density(data = BroadDeciduous, 
+               mapping = aes(x = Tgs_min, fill='Broadleaf Deciduous', color='Broadleaf Deciduous'), size=1, alpha=0.1)+
+  geom_density(data = Monocot, 
+               mapping = aes(x = Tgs_min, fill='Monocot', color='Monocot'), size=1, alpha=0.1)+
+  geom_density(data = Succulent, 
+               mapping = aes(x = Tgs_min, fill='Succulent', color='Succulent'), size=1, alpha=0.1)+
+  geom_density(data = Needle, 
+               mapping = aes(x = Tgs_min, fill='Needleleaf', color='Needleleaf'), size=1, alpha=0.1)+
+  scale_fill_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                         "Broadleaf Deciduous" = "green",
+                                         "Monocot" = "yellow",
+                                         "Succulent" = "purple",
+                                         "Needleleaf" = "blue"))+
+  scale_color_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                          "Broadleaf Deciduous" = "dark green",
+                                          "Monocot" = "orange",
+                                          "Succulent" = "purple",
+                                          "Needleleaf" = "blue"))+
+  scale_x_continuous(name= "Minimum Growing Season Temperature", breaks=seq(0, 36, by = 2))+
+  scale_y_continuous(name= "Tree Species Density")
+
+ggplot() + 
+  geom_density(data = BroadEvergreens, 
+               mapping = aes(x = Tgs_max, fill='Broadleaf Evergreen', color='Broadleaf Evergreen'),size=1, alpha=0.1)+
+  geom_density(data = BroadDeciduous, 
+               mapping = aes(x = Tgs_max, fill='Broadleaf Deciduous', color='Broadleaf Deciduous'), size=1, alpha=0.1)+
+  geom_density(data = Monocot, 
+               mapping = aes(x = Tgs_max, fill='Monocot', color='Monocot'), size=1, alpha=0.1)+
+  geom_density(data = Succulent, 
+               mapping = aes(x = Tgs_max, fill='Succulent', color='Succulent'), size=1, alpha=0.1)+
+  geom_density(data = Needle, 
+               mapping = aes(x = Tgs_max, fill='Needleleaf', color='Needleleaf'), size=1, alpha=0.1)+
+  scale_fill_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                         "Broadleaf Deciduous" = "green",
+                                         "Monocot" = "yellow",
+                                         "Succulent" = "purple",
+                                         "Needleleaf" = "blue"))+
+  scale_color_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                          "Broadleaf Deciduous" = "dark green",
+                                          "Monocot" = "orange",
+                                          "Succulent" = "purple",
+                                          "Needleleaf" = "blue"))+
+  scale_x_continuous(name= "Maximum Growing Season Temperature", breaks=seq(0, 36, by = 2))+
+  scale_y_continuous(name= "Tree Species Density")
+
+
+
+ggplot() + 
+  geom_density2d(data = BroadEvergreens, 
+               mapping = aes(x = Tgs_min, y = Tclx_min, fill='Broadleaf Evergreen', color='Broadleaf Evergreen'),size=1, alpha=0.8)+
+  geom_density2d(data = BroadDeciduous, 
+               mapping = aes(x = Tgs_min, y = Tclx_min, fill='Broadleaf Deciduous', color='Broadleaf Deciduous'), size=1, alpha=0.8)+
+  geom_density2d(data = Monocot, 
+               mapping = aes(x = Tgs_min, y = Tclx_min, fill='Monocot', color='Monocot'), size=1, alpha=0.8)+
+  geom_density2d(data = Succulent, 
+               mapping = aes(x = Tgs_min, y = Tclx_min, fill='Succulent', color='Succulent'), size=1, alpha=0.8)+
+  geom_density2d(data = Needle, 
+               mapping = aes(x = Tgs_min, y = Tclx_min, fill='Needleleaf', color='Needleleaf'), size=1, alpha=0.8)+
+  scale_fill_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                         "Broadleaf Deciduous" = "green",
+                                         "Monocot" = "yellow",
+                                         "Succulent" = "purple",
+                                         "Needleleaf" = "blue"))+
+  scale_color_manual("Legend", values = c("Broadleaf Evergreen" = "red",
+                                          "Broadleaf Deciduous" = "dark green",
+                                          "Monocot" = "orange",
+                                          "Succulent" = "purple",
+                                          "Needleleaf" = "blue"))+
+  scale_x_continuous(name= "Minimum Growing Season Temperature", breaks=seq(0, 36, by = 2))+
+  scale_y_continuous(name= "Minnimum Annual Extreme Low", breaks=seq(-65, 5, by = 5))
+
+library(BiodiversityR)
+library(ape)
+library(cluster)
+
+dist <- vegdist(TreeClim[,4:7], method = 'gower')
+clust <- hclust(dist)
+plot(clust, cex = 0.1)
+cuttree <- as.data.frame(cutree(clust,5))
+cuttree$file <- rownames(cuttree)
+colnames(cuttree) <- c('cluster', 'file')
+TreeClimcut <- merge(TreeClim, cuttree, by='file')
+TreeClimcut$cluster <- as.character(TreeClimcut$cluster)
+ggplot() + 
+  geom_density2d(data = TreeClimcut, 
+                 mapping = aes(x = Tgs_min, y = Tclx_min, fill=cluster, color=cluster),size=1, alpha=0.8)+
+  geom_point(data = TreeClimcut, 
+                 mapping = aes(x = Tgs_min, y = Tclx_min, fill=cluster, color=cluster),size=1, alpha=0.8)+
+  scale_fill_manual("Legend", values = c("1" = "red",
+                                         "2" = "green",
+                                         "3" = "blue",
+                                         "4" = "orange",
+                                         "5" = "yellow",
+                                         "6" = "purple",
+                                         "7" = "pink"))+
+  scale_color_manual("Legend", values = c("1" = "red",
+                                          "2" = "green",
+                                          "3" = "blue",
+                                          "4" = "orange",
+                                          "5" = "yellow",
+                                          "6" = "purple",
+                                          "7" = "pink"))+
+  scale_x_continuous(name= "Minimum Growing Season Temperature", breaks=seq(0, 36, by = 2))+
+  scale_y_continuous(name= "Minnimum Annual Extreme Low", breaks=seq(-65, 5, by = 5))
+
+
+ggplot() + 
+  geom_boxplot(data = TreeClimcut, 
+               mapping = aes(x = cluster, y = Tgs_min, fill=cluster, color=cluster),size=1, alpha=0.5)+
+  scale_fill_manual("Legend", values = c("1" = "red",
+                                         "2" = "green",
+                                         "3" = "blue",
+                                         "4" = "orange",
+                                         "5" = "yellow",
+                                         "6" = "purple",
+                                         "7" = "pink"))+
+  scale_color_manual("Legend", values = c("1" = "red",
+                                          "2" = "green",
+                                          "3" = "blue",
+                                          "4" = "orange",
+                                          "5" = "yellow",
+                                          "6" = "purple",
+                                          "7" = "pink"))+
+  scale_y_continuous(name= "Minimum Growing Season Temperature", breaks=seq(0, 36, by = 2))
+#scale_y_continuous(name= "Minnimum Annual Extreme Low", breaks=seq(-65, 5, by = 5))
+ggplot() + 
+  geom_boxplot(data = TreeClimcut, 
+               mapping = aes(x = cluster, y = Tgs_max, fill=cluster, color=cluster),size=1, alpha=0.5)+
+  geom_boxplot(data = TreeClimcut, 
+               mapping = aes(x = cluster, y = Tgs_min, fill=cluster, color=cluster),size=1, alpha=0.5)+
+  scale_fill_manual("Legend", values = c("1" = "red",
+                                         "2" = "green",
+                                         "3" = "blue",
+                                         "4" = "orange",
+                                         "5" = "yellow",
+                                         "6" = "purple",
+                                         "7" = "pink"))+
+  scale_color_manual("Legend", values = c("1" = "red",
+                                          "2" = "green",
+                                          "3" = "blue",
+                                          "4" = "orange",
+                                          "5" = "yellow",
+                                          "6" = "purple",
+                                          "7" = "pink"))+
+  scale_y_continuous(name= "Maximum Growing Season Temperature", breaks=seq(0, 36, by = 2))
+#scale_y_continuous(name= "Minnimum Annual Extreme Low", breaks=seq(-65, 5, by = 5))
+
+ggplot() + 
+  geom_boxplot(data = TreeClimcut, 
+               mapping = aes(x = cluster, y = Tclx_min, fill=cluster, color=cluster),size=1, alpha=0.5)+
+  scale_fill_manual("Legend", values = c("1" = "red",
+                                         "2" = "green",
+                                         "3" = "blue",
+                                         "4" = "orange",
+                                         "5" = "yellow",
+                                         "6" = "purple",
+                                         "7" = "pink"))+
+  scale_color_manual("Legend", values = c("1" = "red",
+                                          "2" = "green",
+                                          "3" = "blue",
+                                          "4" = "orange",
+                                          "5" = "yellow",
+                                          "6" = "purple",
+                                          "7" = "pink"))+
+  #scale_y_continuous(name= "Minimum Growing Season Temperature", breaks=seq(0, 36, by = 2))
+scale_y_continuous(name= "Minnimum Annual Extreme Low", breaks=seq(-65, 5, by = 5))
+aggregate(TreeClimcut[,c('Tclx_max','Tclx_min','Tgs_max','Tgs_min')], by=list(TreeClimcut$cluster), FUN='median')
+aggregate(TreeClimcut[,c('Tclx_max','Tclx_min','Tgs_max','Tgs_min')], by=list(TreeClimcut$cluster), FUN='sd')
+
+
+library(rpart)
+library(rpart.plot)
+model <- rpart(cluster~ Tclx_min +Tclx_max +Tgs_min +Tgs_max, data= TreeClimcut, method = "class", maxdepth = 4)
+rpart.plot(model, extra=108)
